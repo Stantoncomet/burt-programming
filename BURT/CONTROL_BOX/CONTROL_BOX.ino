ffa25bf514a59396b076d668c5769ab62d96a856
Stantoncomet <96146851+Stantoncomet@users.noreply.github.com>
2024-01-18 15:43:28 -0800

    main control box code

--- BURT/CONTROL_BOX/CONTROL_BOX.ino
+++ BURT/CONTROL_BOX/CONTROL_BOX.ino
@@ -1,65 +1,43 @@
-#include <RFM69.h>
+//LIBRARIES
+#include <XBOXONE.h>
 #include <SPI.h>
+#include "controlsMap.h"
 
-RFM69 radio;
-//comment!
-const int DOWNBUTTONPIN = 9;
+//OBJECTS
 
-int buttonState;
-int lastButtonState = LOW; 
-
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50; 
-
-
+//Xbox Controller
+USB Usb;
+XBOXONE Xbox(&Usb);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  setupController();
 
-  pinMode(DOWNBUTTONPIN, INPUT);
-
-  radio.initialize(RF69_915MHZ, 3, 0);
-  radio.setHighPower();
 }
 
 
 void loop() {
-  if (radio.receiveDone()) {
-    static char time_team[16];
+  //CONTROLLER
+  Usb.Task();
 
-    for (byte i = 0; i < radio.DATALEN; i++)
-      strcat(time_team, (char)radio.DATA[i]);
-    
-    write_to_monitor(time_team);
-  }
+  if (Xbox.XboxOneConnected) {
 
-  //sends "1" to the controlbox if the button is pressed
-  if (digitalRead(DOWNBUTTONPIN)) {
-
-  }
-
-
-  int reading = digitalRead(DOWNBUTTONPIN);
-
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis(); // reset the debouncing timer
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      if (buttonState == HIGH) {
-        //should work, sends a signal if buttonstate is high
-        radio.send(1, "1", 1);
-        Serial.println("sent signal 1");
-      }
+    //MOVEMENT
+    if (Xbox.getButtonPress(MAP_RISE)) {
+      //go up
+      Serial.println("upping");
     }
+
+    if (Xbox.getButtonPress(MAP_FALL)) {
+      //go down
+      Serial.println("downing");
+    }
+
+    if (forward()) {
+      Serial.print("choo choo  ");
+      Serial.println(Xbox.getAnalogHat(MAP_STRAFE));
+    }
+
+    delay(1); //REPLACE WITH NOT THIS
   }
-}
-
-
-void write_to_monitor(char* monitor_string) {
-  Serial.print(monitor_string); //print(), not println()
 }
